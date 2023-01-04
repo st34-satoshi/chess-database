a96cfbd84fcbd04f621898aba8c2f51acb520aad
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-01-04 18:53:50 +0900

    update: user name is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
+    # TODO: error message, creating the same name user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_162900) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
