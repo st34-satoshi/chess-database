308425eb7a1593f34aa07d4eb0f69743d8451199
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-01-08 13:03:45 +0900

    add: hello chess.js

--- app/javascript/game/show.js
+++ app/javascript/game/show.js
@@ -1,7 +1,23 @@
 import "application"
 import "./chessboard"
+// import "./chess"
+import { Chess } from './chess-0-13-4.js'
+
+function helloChess(){
+    console.log('hello chess');
+    const chess = new Chess()
+
+    while (!chess.game_over()) {
+        const moves = chess.moves()
+        const move = moves[Math.floor(Math.random() * moves.length)]
+        chess.move(move)
+    }
+    console.log(chess.pgn())
+
+}
 
 $(function() {
     console.log('hello game show');
-    var board1 = Chessboard('board1', 'start')
+    var board1 = Chessboard('board1', 'start');
+    helloChess();
 })
