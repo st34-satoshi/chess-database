85b5112468c128de10cdd820be156404078c682f
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-01-08 17:21:36 +0900

    update: show current position on the board

--- app/javascript/game/show.js
+++ app/javascript/game/show.js
@@ -3,7 +3,7 @@ import "./chessboard"
 // import "./chess"
 import { Chess } from './chess-0-13-4.js'
 
-function helloChess(){
+function helloChess(board){
     const chess = new Chess()
 
     const moves = $('#gameMoves').text();
@@ -11,11 +11,12 @@ function helloChess(){
     console.log(moves);
     chess.load_pgn(moves);
     console.log(chess.pgn())
-
+    console.log(chess.fen())
+    board.position(chess.fen());
 }
 
 $(function() {
     console.log('hello game show');
     var board1 = Chessboard('board1', 'start');
-    helloChess();
+    helloChess(board1);
 })
