ec046b2aea84097056330c4b0bc7c590a97190d8
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-03-01 20:44:17 +0900

    datalist for white name

--- app/javascript/game/form.js
+++ app/javascript/game/form.js
@@ -2,13 +2,31 @@
  * used for games/_form.html.erb
  */
 
+let NameDict = null;
+
 export function SetOnChange(){
     $('#white_name_input').on('change', function () {
         // get selected user id
         const name = $(this).val()
-        const nameDict = $('#white-name-list').data('user-id-table')
-        const id = nameDict[name]
+        if(NameDict === null) {
+            return;
+        }
+        const id = NameDict[name]
         // set game_white_id
         $('#game_white_id').val(id)
     });
+    $('#black_name_input').on('change', function () {
+        // get selected user id
+        const name = $(this).val()
+        if(NameDict === null) {
+            return;
+        }
+        const id = NameDict[name]
+        // set game_black_id
+        $('#game_black_id').val(id)
+    });
 }
+
+$(function() {
+    NameDict = $('#white-name-list').data('user-id-table')
+})
