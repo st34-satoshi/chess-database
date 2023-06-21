9702416a6a9aa5cb4e4b51e5525c127a9cc7ee86
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-06-21 23:46:49 +0900

    win lose badge

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,16 @@
 class Game < ApplicationRecord
   belongs_to :white, class_name: "Player", foreign_key: "white_id"
   belongs_to :black, class_name: "Player", foreign_key: "black_id"
+
+  def white_win?
+    result == "White Win"
+  end
+
+  def black_win?
+    result == "Black Win"
+  end
+
+  def draw?
+    result == "Draw"
+  end
 end
