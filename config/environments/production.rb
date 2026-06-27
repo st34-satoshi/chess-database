947913e3384c23b1abf3a2db34d8eadc9e315940
Satoshi Tanaka <st34.satoshi@gmail.com>
2026-06-27 19:55:06 +0900

    ログローテート

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.logger = Logger.new('log/production.log', 'monthly')
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
